35249bc01020957effcfc5a24fb7e9525a76f14e
Aiden Li <aidenliw@hotmail.com>
2024-06-14 17:37:54 -0400

    Add camera angel initialzation function.

--- controllers/camera.js
+++ controllers/camera.js
@@ -57,6 +57,28 @@ router.post("/deactivate", async function(req, res)
 	res.render("camera", req.TPL); 
 });
 
+router.post("/initialize", async function(req, res)
+{	
+    let channel = process.env.PUBNUB_CHANNEL;
+    let title = "camera";
+    let command = "initialize";
+    let message = { title: title, command: command }
+    var status = await PubNubModel.publishMessage(channel, message);
+    // console.log(status);
+    // check if the status is successful
+    if (!status.error && status.statusCode === 200){
+        req.TPL.succeed_message = "Camera Angel Initialized!";
+        req.TPL.blue = true;
+        // create a log
+        let log = req.session.username + " initializes camera angel.";
+        await LogModel.createLog(log);
+    } else {
+        req.TPL.failed_message = "Initializing camera angel failed! Please try again later.";
+    }
+    req.TPL.degree = currentDegree;
+	res.render("camera", req.TPL); 
+});
+
 router.post("/:direction", async function(req, res)
 {	
     const direction = req.params.direction;
